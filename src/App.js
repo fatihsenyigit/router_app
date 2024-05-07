02252c0a4223f20746f91fa3bbdb6344bba530ae
fatih <fsenyigit8@gmail.com>
2024-05-07 12:45:38 +0300

    login form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import LoginForm from './components/LoginForm';
 
 function App() {
   return (
     <div className="App">
-      
+      <LoginForm></LoginForm>
     </div>
   );
 }
--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 const LoginForm = () => {
+  const [user, setUser] = useState(false)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setUser(true)
+  }
+
+  console.log(user)
   return (
-    <div>LoginForm</div>
-  )
+    <div>
+      <Container>
+        <Navbar className="bg-success">
+          <h4 className=" ms-3 text-white">Router_APP</h4>
+        </Navbar>
+        <Form className='w-50 my-3' onSubmit={handleSubmit}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </Container>
+    </div>
+  );
 }
 
 export default LoginForm
