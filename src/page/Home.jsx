84bdd906d7fdbe0119fd98504471a07a5386eda7
fatih <fsenyigit8@gmail.com>
2024-05-08 20:00:51 +0300

    loading

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -6,18 +6,23 @@ import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import loading from '../assets/loading.gif'
 
 const Home = () => {
       const [followers, setFollowers] = useState()
+      const [isLoading, setIsLoading] = useState(true)
  
 
     const getFollowers = () => {
+      setIsLoading(true)
         fetch(
           "https://api.github.com/users/anthonyharold67/followers?per_page=100",
         )
           .then((res) => res.json())
-          .then((data) => setFollowers(data))
-          .catch((err) => console.log(err));
+          .then((data) =>  {return (setFollowers(data), setIsLoading(false))})
+          .catch((err) => {
+            return console.log(err), setIsLoading(true);
+          });
     }
 
      useEffect(() => {
@@ -33,26 +38,43 @@ const Home = () => {
           <InputGroup.Text id="basic-addon1">search</InputGroup.Text>
           <Form.Control aria-label="search" aria-describedby="basic-addon1" />
         </InputGroup>
-        <Row xs={1} sm={2} md={3} lg={4} xl={5} xxl={6} className="d-flex flex-wrap">
-          {followers?.map((item, index) => (
-            <Col key={item.id} className="d-flex justify-content-center mt-3">
-              <Card
-                className="d-flex justify-content-center align-items-center text-center pt-3"
-                style={{ width: "12rem" }}
-              >
-                <Card.Img
-                  variant="top"
-                  src={item.avatar_url}
-                  style={{ width: "150px" }}
-                />
-                <Card.Body>
-                  <Card.Title>{item.login}</Card.Title>
+        <Row
+          xs={1}
+          sm={2}
+          md={3}
+          lg={4}
+          xl={5}
+          xxl={6}
+          className="d-flex flex-wrap"
+        >
+          {isLoading ? (
+            <img src={loading} alt="loading" />
+          ) : (
+            <>
+              {followers?.map((item, index) => (
+                <Col
+                  key={item.id}
+                  className="d-flex justify-content-center mt-3"
+                >
+                  <Card
+                    className="d-flex justify-content-center align-items-center text-center pt-3"
+                    style={{ width: "12rem" }}
+                  >
+                    <Card.Img
+                      variant="top"
+                      src={item.avatar_url}
+                      style={{ width: "150px" }}
+                    />
+                    <Card.Body>
+                      <Card.Title>{item.login}</Card.Title>
 
-                  <Button variant="primary">details</Button>
-                </Card.Body>
-              </Card>
-            </Col>
-          ))}
+                      <Button variant="primary">details</Button>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              ))}
+            </>
+          )}
         </Row>
       </Container>
     </div>
