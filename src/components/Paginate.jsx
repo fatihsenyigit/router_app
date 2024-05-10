562c8e54dc25e8ad200b6911916df01aa130b9d9
fatih <fsenyigit8@gmail.com>
2024-05-10 19:01:34 +0300

    searching sych with page number

--- src/components/Paginate.jsx
+++ src/components/Paginate.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import Pagination from "react-bootstrap/Pagination";
 
-const Paginate = () => {
+const Paginate = ({ setActivePage, activePage, totalPages }) => {
+  const handlePageChange = (pageNumber) => {
+    setActivePage(pageNumber);
+  };
+
+  let items = [];
+  for (let number = 1; number <= totalPages; number++) {
+    items.push(
+      <Pagination.Item
+        key={number}
+        active={number === activePage}
+        onClick={() => handlePageChange(number)}
+      >
+        {number}
+      </Pagination.Item>,
+    );
+  }
+
   return (
-    <div>Paginate</div>
-  )
-}
-
-export default Paginate
+    <div>
+      <Pagination>{items}</Pagination>
+    </div>
+  );
+};
+export default Paginate;
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,84 +1,125 @@
-import React, { useEffect, useState } from 'react'
-import NavBar from '../components/NavBar'
-import { Outlet } from 'react-router-dom'
-import { Col, Container, Row } from 'react-bootstrap'
+import React, { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
+import { Outlet } from "react-router-dom";
+import { Col, Container, Row } from "react-bootstrap";
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
-import loading from '../assets/loading.gif'
+import loading from "../assets/loading.gif";
+import Paginate from "../components/Paginate";
 
 const Home = () => {
-      const [followers, setFollowers] = useState()
-      const [isLoading, setIsLoading] = useState(true)
- 
+  const [isLoading, setIsLoading] = useState(false);
+  const [followers, setFollowers] = useState();
+  const [activePage, setActivePage] = useState(1);
+  const [searchData, setSearchData] = useState("");
+  // const [showFollowers, setShowFollowers] = useState();
+  const perPage = 6;
+  const startIndex = (activePage - 1) * perPage;
 
-    const getFollowers = () => {
-      setIsLoading(true)
-        fetch(
-          "https://api.github.com/users/anthonyharold67/followers?per_page=100",
-        )
-          .then((res) => res.json())
-          .then((data) =>  {return (setFollowers(data), setIsLoading(false))})
-          .catch((err) => {
-            return console.log(err), setIsLoading(true);
-          });
-    }
+  const totalPages =
+    searchData.trim() === ""
+      ? Math.ceil(followers?.length / perPage)
+      : Math.ceil(
+          (followers?.filter((e) =>
+            e.login.toLowerCase().includes(searchData.trim().toLowerCase()),
+          )).length / perPage,
+        );
 
-     useEffect(() => {
-       getFollowers();
-     }, []);
+  console.log(totalPages);
+
+  // const totalPages = Math.ceil(followers?.length / perPage);
+
+  const showFollowers =
+    searchData.trim() === ""
+      ? followers?.slice(startIndex, startIndex + perPage)
+      : followers
+          ?.filter((e) =>
+            e.login.toLowerCase().includes(searchData.trim().toLowerCase()),
+          )
+          .slice(startIndex, startIndex + perPage);
+
+  // const showFollowers = followers?.slice(startIndex, startIndex + perPage);
+
+  function getFollowers() {
+    setIsLoading(true);
+    fetch("https://api.github.com/users/anthonyharold67/followers?per_page=100")
+      .then((res) => res.json())
+      .then((data) => {
+        return setFollowers(data), setIsLoading(false);
+      })
+      .catch((err) => {
+        return console.log(err), setIsLoading(true);
+      });
+  }
+
+  useEffect(() => {
+    getFollowers();
+  }, []);
+
+  const handleChange = (e) => {
+    setSearchData(e.target.value);
+  };
 
   return (
     <div>
       <NavBar />
       <Outlet></Outlet>
-      <Container className=" mt-4">
-        <InputGroup className="mb-3 w-50 m-auto">
-          <InputGroup.Text id="basic-addon1">search</InputGroup.Text>
-          <Form.Control aria-label="search" aria-describedby="basic-addon1" />
-        </InputGroup>
-        <Row
-          xs={1}
-          sm={2}
-          md={3}
-          lg={4}
-          xl={5}
-          xxl={6}
-          className="d-flex flex-wrap"
-        >
-          {isLoading ? (
-            <img src={loading} alt="loading" />
-          ) : (
-            <>
-              {followers?.map((item, index) => (
-                <Col
-                  key={item.id}
-                  className="d-flex justify-content-center mt-3"
+      {isLoading ? (
+        <div>
+          <img src={loading} alt="loading" style={{ width: "200px" }} />
+        </div>
+      ) : (
+        <Container className=" mt-4">
+          <InputGroup className="mb-3 w-50 m-auto ">
+            <InputGroup.Text id="basic-addon1">search</InputGroup.Text>
+            <Form.Control
+              onChange={handleChange}
+              aria-label="search"
+              aria-describedby="basic-addon1"
+            />
+          </InputGroup>
+          <Row
+            xs={1}
+            sm={2}
+            md={3}
+            lg={4}
+            xl={5}
+            xxl={6}
+            // className="d-flex  "
+          >
+            {showFollowers?.map((item, index) => (
+              <Col key={item.id} className="mt-3">
+                <Card
+                  className="d-flex justify-content-center align-items-center text-center pt-3"
+                  style={{ width: "12rem" }}
                 >
-                  <Card
-                    className="d-flex justify-content-center align-items-center text-center pt-3"
-                    style={{ width: "12rem" }}
-                  >
-                    <Card.Img
-                      variant="top"
-                      src={item.avatar_url}
-                      style={{ width: "150px" }}
-                    />
-                    <Card.Body>
-                      <Card.Title>{item.login}</Card.Title>
+                  <Card.Img
+                    variant="top"
+                    src={item.avatar_url}
+                    style={{ width: "150px" }}
+                  />
+                  <Card.Body>
+                    <Card.Title>{item.login}</Card.Title>
 
-                      <Button variant="primary">details</Button>
-                    </Card.Body>
-                  </Card>
-                </Col>
-              ))}
-            </>
-          )}
-        </Row>
-      </Container>
+                    <Button variant="primary">details</Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+          <div className="mt-4 d-flex justify-content-center">
+            <Paginate
+              activePage={activePage}
+              setActivePage={setActivePage}
+              totalPages={totalPages}
+            />
+          </div>
+        </Container>
+      )}
     </div>
   );
-}
+};
 
-export default Home
+export default Home;
